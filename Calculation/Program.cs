ecf46dcfb3966b50228e8b59a16332eb0d468eee
Kalyan102003 <159612759+Kalyan102003@users.noreply.github.com>
2026-03-06 15:37:20 +0530

    Added substraction Code

--- Calculation/Program.cs
+++ Calculation/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Calculation
+﻿using System;
+
+namespace Calculation
 {
     internal class Program
     {
@@ -9,6 +11,7 @@
 
             Substraction substraction = new Substraction();
             substraction.DoSubstraction(10, 5);
+           
 
             Multiplication multiplication = new Multiplication();
             multiplication.DoMultiplication(5, 10);
@@ -16,5 +19,18 @@
             Division division = new Division();
             division.DoDivision(5, 10);
         }
+
+       
+        public class Substraction
+        {
+            public int SubstractionOutput = 0;
+
+            public int DoSubstraction(int input1, int input2)
+            {
+                SubstractionOutput = input1 - input2;
+                return SubstractionOutput;
+            }
+        }
+
     }
 }
