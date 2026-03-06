45c80b6bd612a674a86f8b13ffda13df38fa1fa3
Murugesh_H <murugesh@monocept.com>
2026-03-06 14:43:36 +0530

    Add project files.

---
+++ Calculation/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Calculation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Addition addition = new Addition();
+            addition.DoAddition(5, 10);
+
+            Substraction substraction = new Substraction();
+            substraction.DoSubstraction(10, 5);
+
+            Multiplication multiplication = new Multiplication();
+            multiplication.DoMultiplication(5, 10);
+
+            Division division = new Division();
+            division.DoDivision(5, 10);
+        }
+    }
+}
